a2b66442c019952d2ff4f908da053b6fdf81d102
rupabarma <108084091+rupabarma@users.noreply.github.com>
2023-03-19 20:52:54 +0530

    Update Styles.ts

--- src/components/chatMessage/Styles.ts
+++ src/components/chatMessage/Styles.ts
@@ -3,12 +3,14 @@ import { StyleSheet } from "react-native";
 const Styles = StyleSheet.create({
     container:{
         flex:1,
-        padding: 10
+        padding: 10,
+        flexDirection: 'row',
     },
     messageBox:{
         display: 'flex',
         borderRadius: 8,
-        padding: 10
+        padding: 10,
+        flexDirection: 'row',
     },
     name:{
         fontSize: 15,
@@ -17,13 +19,15 @@ const Styles = StyleSheet.create({
     message:{
         fontSize: 18,
         fontWeight: 'normal',
-        color: '#000'
+        color: '#000',
+        padding: 5
     },
     time:{
         fontSize: 13,
         fontWeight: 'normal',
         color: '#4d4d4d',
         alignSelf: 'flex-end',
+        padding: 5,
     },
 })
 
